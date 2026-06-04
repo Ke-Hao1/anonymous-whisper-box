11621be2070a9f67d680e9009a7b29322b1062ae
Ke-Hao1 <ssyks4@nottingham.edu.cn>
2026-06-04 13:23:05 +0800

    Update Supabase configuration in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 毕业匿名留言板 v6 配置
 // 必须填 Supabase 信息，才能让所有人跨手机稳定使用。
 window.MAILBOX_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: ""
+  SUPABASE_URL: "https://garnetwxrubtrxioqhfq.supabase.co/rest/v1/",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_G0mBz3_cOQ7B7VhDCmCZsQ_pI24UGPv"
 };
